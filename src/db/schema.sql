13005278075ffd3202e20cf63c3a53e978c2bcc7
Andrew Dawson <andrewjdawson2016@gmail.com>
2023-12-29 12:28:21 -0800

    rename references from users to app_user

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -13,14 +13,14 @@ CREATE TABLE building (
     nickname VARCHAR(255) NOT NULL UNIQUE,
     building_type VARCHAR(15) NOT NULL CHECK (building_type IN ('SINGLE_FAMILY', 'MULTI_FAMILY')),
     first_rental_month VARCHAR(7) NOT NULL,
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE unit (
     id SERIAL PRIMARY KEY,
     building_id INT NOT NULL REFERENCES building(id) ON DELETE CASCADE, 
     unit_number VARCHAR(255),
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE expense (
@@ -29,7 +29,7 @@ CREATE TABLE expense (
     month_year VARCHAR(7) NOT NULL,
     amount INT NOT NULL,
     note TEXT DEFAULT '',
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE lease (
@@ -40,7 +40,7 @@ CREATE TABLE lease (
     price_per_month INT NOT NULL,
     is_renewal BOOLEAN NOT NULL DEFAULT false,
     CHECK (end_date > start_date),
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE lease_note (
@@ -48,7 +48,7 @@ CREATE TABLE lease_note (
     lease_id INT NOT NULL REFERENCES lease(id) ON DELETE CASCADE,
     note TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE lease_event (
@@ -57,21 +57,21 @@ CREATE TABLE lease_event (
     due_date DATE NOT NULL,
     execution_date DATE,
     description VARCHAR(10) NOT NULL CHECK (description IN ('START', 'SIX_MONTH', 'TWO_MONTH', 'ONE_MONTH', 'END')),
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE tenant (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE tenant_lease (
     tenant_id INT NOT NULL REFERENCES tenant(id) ON DELETE CASCADE,
     lease_id INT NOT NULL REFERENCES lease(id) ON DELETE CASCADE,
     PRIMARY KEY (tenant_id, lease_id),
-    user_id INTEGER REFERENCES users(id)
+    user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE INDEX idx_lease_event_due_date ON lease_event(due_date);
